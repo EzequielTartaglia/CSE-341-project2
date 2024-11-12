bce997cdf0769632ae8bc1608b0046c9d743c147
EzequielTartaglia <ezequieltartaglia@churchofjesuschrist.org>
2024-11-12 16:22:27 -0300

    Continue later

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const routes = require('express').Router();
 
+routes.use('/', require('./swagger'));
 routes.use('/movies', require('./movies'));
 routes.use('/movie_genders', require('./movie_genders'));
 
 module.exports = routes;
-ts = routes;
