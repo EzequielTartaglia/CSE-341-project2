59f474dd2c5d4c590ec9fdf3314195a651db9929
EzequielTartaglia <ezequieltartaglia@churchofjesuschrist.org>
2024-11-12 11:35:08 -0300

    Add changes

--- models/movie_genders.js
+++ models/movie_genders.js
@@ -3,7 +3,7 @@ module.exports = (mongoose) => {
     'movie_genders',
     mongoose.Schema(
       {
-        movie_gender_id: { type: Number },
+        movie_gender_id: { type: mongoose.Schema.Types.ObjectId, required: true },
         name: { type: String, required: true, unique: true }
       },
       { timestamps: true }
