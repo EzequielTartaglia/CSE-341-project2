6bd667467c6bed274968ef66c41f755a038d48da
EzequielTartaglia <ezequielmtartaglia@gmail.com>
2024-11-13 01:15:30 -0300

    Control

--- routes/movies.js
+++ routes/movies.js
@@ -6,12 +6,12 @@ const validation = require('../middleware/validate');
 
 router.get('/', moviesController.getAll);
 
-router.get('/:movie_id', moviesController.getSingle);
+router.get('/:id', moviesController.getSingle);
 
 router.post('/', validation.saveMovie, moviesController.createMovie);
 
-router.put('/:movie_id', validation.saveMovie, moviesController.updateMovie);
+router.put('/:id', validation.saveMovie, moviesController.updateMovie);
 
-router.delete('/:movie_id', moviesController.deleteMovie);
+router.delete('/:id', moviesController.deleteMovie);
 
 module.exports = router;
